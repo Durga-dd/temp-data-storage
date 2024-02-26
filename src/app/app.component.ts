1f63d14dad9069dcc47b923ec495dc95a960cf0e
Durga-dd <s.durgadevi@kuwy.in>
2024-02-26 15:37:44 +0530

    Temporary data storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import {
+  Validators,
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  FormArray,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'temp-formdata-storage';
+  tempform!: FormGroup;
+  user_data: any = [];
+  getData: any = [];
+  isHeaderVisible: boolean = false;
+  constructor(private fb: FormBuilder) {
+    this.tempform = this.fb.group({
+      firstname: [''],
+      lastname: [''],
+      phone_num: ['', Validators.pattern('^[0-9 ]{10}$')],
+    });
+  }
+  onSubmit() {
+    this.user_data.push(this.tempform.value);
+    localStorage.setItem('phonebook', JSON.stringify(this.user_data));
+    this.getTempData();
+  }
+
+  getTempData() {
+    let data: any = localStorage.getItem('phonebook');
+    this.getData = JSON.parse(data);
+    this.isHeaderVisible = this.getData ? true : false;
+    console.log(this.getData);
+    return this.getData;
+  }
 }
